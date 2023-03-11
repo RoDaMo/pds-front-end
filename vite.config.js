ac60a2afc7269cceeffdae73a505b547c7f7c28c
Caioferrari04 <caioferrari0484@gmail.com>
2023-03-11 15:40:18 -0300

    Initial bootstrap config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 // import mpa from 'vite-plugin-mpa'
 
 export default defineConfig({
   appType: 'mpa',
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap')
+    }
+  },
   build: {
     outDir: 'dist'
   },
