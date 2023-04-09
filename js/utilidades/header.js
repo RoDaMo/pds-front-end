79363822c1ba99dbf9cd1e2b430faacc463bb7fa
Gustavo Ian <gustavoian1790@gmail.com>
2023-04-09 15:16:03 -0300

    feat/fix: conteúdo principal home (POC) / grid arrumado

--- js/utilidades/header.js
+++ js/utilidades/header.js
@@ -4,9 +4,9 @@ export class header extends HTMLElement {
         this.innerHTML = `
             <header>
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container-fluid justify-content-center">
+                    <div class="container justify-content-center">
                         <div class="col col-lg-4">
-                            <a class="navbar-brand m-auto p-auto" href="#"><img src="../public/Logo_Playoffs.png" class="img-fluid" width="180" alt="Logo Playoffs"></a>
+                            <a class="navbar-brand m-auto p-auto" href="#"><img src="Logo_Playoffs.png" class="img-fluid" width="180" alt="Logo Playoffs"></a>
                         </div>
                         
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,7 +18,7 @@ export class header extends HTMLElement {
                             <input class="form-control m-lg-auto p-1 m-sm-5 mb-sm-2 m-3 mb-2 border-0 rounded-pill h-5" type="search" placeholder="Procurar" aria-label="Search">
                             </form>  
 
-                            <ul class="col col-sm-10 col-lg navbar-nav m-auto p-auto justify-content-end">
+                            <ul class="menu-li col col-sm-10 col-lg navbar-nav m-auto p-auto justify-content-end">
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Criar Campeonato</a>
                             </li>
