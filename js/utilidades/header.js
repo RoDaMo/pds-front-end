3067ef01c69e4f9fee57389ee5ee1e77801bbddb
Gustavo Ian <gustavoian1790@gmail.com>
2023-04-11 11:14:14 -0300

    feat: atualização de responsividade na navbar e no conteúdo principal

--- js/utilidades/header.js
+++ js/utilidades/header.js
@@ -4,18 +4,19 @@ export class header extends HTMLElement {
         this.innerHTML = `
             <header>
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container justify-content-center">
+                    <div class="container justify-content-center align-items-center">
                         <div class="col col-lg-4">
                             <a class="navbar-brand m-auto p-auto" href="#"><img src="Logo_Playoffs.png" class="img-fluid" width="180" alt="Logo Playoffs"></a>
                         </div>
                         
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
+                            <span class="btn-close d-flex"></span>
                         </button>
                             
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">   
+                        <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">   
                             <form class="col col-md d-flex justify-content-center m-auto p-auto" role="search">
-                            <input class="form-control m-lg-auto p-1 m-sm-5 mb-sm-2 m-3 mb-2 border-0 rounded-pill h-5" type="search" placeholder="Procurar" aria-label="Search">
+                            <input class="form-control m-lg-auto p-1 mt-5 m-sm-5 mb-sm-2 m-3 mb-2 border-0 rounded-pill h-5" type="search" placeholder="Procurar" aria-label="Search">
                             </form>  
 
                             <ul class="menu-li col col-sm-10 col-lg navbar-nav m-auto p-auto justify-content-end">
