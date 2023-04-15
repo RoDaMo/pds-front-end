71576772658e0644dbf51fd1800d95fa215614ed
Caioferrari04 <caioferrari0484@gmail.com>
2023-04-15 18:52:00 -0300

    feat: implementando internacionalizacao

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+
+import { inicializarInternacionalizacao } from './js/utilidades/internacionalizacao'
+import globalEn from './js/i18n/en/main.json' assert { type: 'JSON' };
+import globalPt from './js/i18n/ptbr/main.json' assert { type: 'JSON' };
 import '/scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
+if (!localStorage.getItem('lng')) {
+  localStorage.setItem('lng', 'ptbr')
+}
+
+document.addEventListener('DOMContentLoaded', () => inicializarInternacionalizacao(globalEn, globalPt))
