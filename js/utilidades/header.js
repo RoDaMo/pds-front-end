e6a68b2cb90e5703470111a32e83b58b92f4e33f
Gustavo Ian <gustavoian1790@gmail.com>
2023-04-22 13:27:27 -0300

    feat: Aplicando efeitos na UI

--- js/utilidades/header.js
+++ js/utilidades/header.js
@@ -13,23 +13,24 @@ export class header extends HTMLElement {
                         <span class="navbar-toggler-icon"></span>
                         <span class="btn-close d-flex"></span>
                     </button>
-                       
+                    <div class="vw-100">   
                     <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">   
                         <form class="col col-md d-flex justify-content-center m-auto p-auto" id="pesquisa" role="search">
                             <input id="barra-pesquisa" class="form-control m-lg-auto p-1 mt-5 m-sm-5 mb-sm-2 m-3 mb-2 border-0 rounded-pill h-5 pesquisar i18-placeholder" type="search" key="PesquisaPlaceholder" placeholder="Procurar" aria-label="Search">
                         </form>  
 
-                        <ul class="menu-li col col-sm-10 col-lg navbar-nav m-auto p-auto justify-content-end">
-                            <li class="nav-item">
-                                <a class="nav-link i18" href="/pages/cadastro-campeonatos.html" key="LinkCadastroCampeonato">Criar Campeonato</a>
+                        <ul class="menu-li col col-sm-10 col-lg navbar-nav m-auto p-auto justify-content-end align-items-center">
+                            <li class="nav-item m-auto">
+                                <a class="nav-link i18 rounded-3 px-3" href="/pages/cadastro-campeonatos.html" key="LinkCadastroCampeonato">Criar Campeonato</a>
                             </li>
                             <li class="nav-item">
-                                <select class="form-select" id="lingua" required>
+                                <select class="form-select rounded-3 ps-3 py-2" id="lingua" required>
                                     <option value="ptbr" ${lng === 'ptbr' ? 'selected' : ''}>Português</option>
                                     <option value="en" ${lng === 'en' ? 'selected' : ''}>English</option>
                                 </select>
                             </li>
                         </ul>
+                    </div>
                     </div>
                 </nav>
             </header>
