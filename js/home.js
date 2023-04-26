74b1e838cbad6099f7ce99771e213585843d095e
Gustavo Ian <gustavoian1790@gmail.com>
2023-04-26 11:37:50 -0300

    feat: Implementando swipe actions e mudança de navbar (mobile)

--- js/home.js
+++ js/home.js
@@ -1,5 +1,25 @@
 "use strict";
 
+const navbar = document.querySelector("componente-header");
+const homeCards = document.querySelectorAll(".home-card.d-flex");
+
+const mobibarClasses = ["position-fixed", "topx-14", "z-1", "bg-white", "start-50", "translate-middle-x", "w-60", "rounded-4"];
+const mobibarLogoClasses = ["w-90", "mb-1", "translate-6-x"];
+
+let mobibarLogo;
+let navTogglerClose;
+let navTogglerOpen;
+
+let count = 0;
+
+document.firstElementChild.scrollIntoView({ block: "start" });
+
+window.onload = () => {
+    mobibarLogo = navbar.querySelector('img[alt="Logo Playoffs"]')
+    navTogglerOpen = navbar.querySelector(".navbar-toggler");
+    navTogglerClose = navbar.querySelector("#close-offcanvas");
+}
+
 // Rellax.JS
 let rellax = new Rellax('.rellax', {
     breakpoints:[576, 768, 1201]
@@ -17,11 +37,65 @@ let rellax = new Rellax('.rellax', {
 
 // observer.observe(document.querySelector("#scroll-trigger"));
 
+// Mobile navbar changer
 // Verificar se ta no começo da página
 window.addEventListener("scroll", () => {
     if (window.scrollY === 0){
-        // remove classList navbar 
+        homeCards.forEach(card => card.classList.remove("ptx-74"));
+        navbar.classList.remove(...mobibarClasses);
+        mobibarLogo.classList.remove(...mobibarLogoClasses);
     } else {
-        // add classList navbar
+        let menuOpen = false;
+
+        homeCards.forEach(card => card.classList.add("ptx-74"));
+
+        navTogglerOpen.addEventListener("click", e => {
+            navbar.classList.remove(...mobibarClasses);  
+            mobibarLogo.classList.remove(...mobibarLogoClasses);
+            menuOpen = true;
+        });
+
+        if (!menuOpen) {
+            navbar.classList.add(...mobibarClasses);
+            mobibarLogo.classList.add(...mobibarLogoClasses);
+        }
+
+        if (window.scrollY > 0) {
+            navTogglerClose.addEventListener("click", e => {
+                navbar.classList.add(...mobibarClasses);
+                mobibarLogo.classList.add(...mobibarLogoClasses);  
+                menuOpen = false;
+            });
+        }
     }
 });
+
+document.addEventListener("swiped-up", e => {
+    e.preventDefault();
+
+    if (!(count >= homeCards.length-1)) {
+        homeCards[count+1].scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    
+        count++;
+        console.log(count);
+    }
+});
+
+document.addEventListener("swiped-down", e => {
+    e.preventDefault();
+
+    if (!(count <= 0)) {
+        homeCards[count-1].scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    
+        count--;
+        console.log(count);
+    }
+});
+
+//74px
--- js/utilidades/header.js
+++ js/utilidades/header.js
@@ -6,7 +6,7 @@ export class header extends HTMLElement {
             <header class="container">
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
                     <div class="col col-lg-4">
-                        <a class="navbar-brand m-auto p-auto" href="/"><img src="/Logo_Playoffs.png" class="img-fluid" width="180" alt="Logo Playoffs"></a>
+                        <a class="navbar-brand m-auto" href="/"><img src="/Logo_Playoffs.png" class="img-fluid" width="180" alt="Logo Playoffs"></a>
                     </div>
                     
                     <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -17,15 +17,15 @@ export class header extends HTMLElement {
 
                         <div class="offcanvas-header container pt-2">
                             <h2 class="fw-bold text-black">Menu</h2>
-                            <button type="button" class="btn-close me-1" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            <button id="close-offcanvas" type="button" class="btn-close me-1" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
 
                         <div class="offcanvas-body">
-                            <form class="col col-md d-flex justify-content-center m-auto p-auto" id="pesquisa" role="search">
+                            <form class="col col-md d-flex justify-content-center m-auto" id="pesquisa" role="search">
                                 <input id="barra-pesquisa" class="form-control m-lg-auto p-1 m-sm-5 mb-sm-2 m-3 mb-2 border-0 rounded-pill h-5 pesquisar i18-placeholder" type="search" key="PesquisaPlaceholder" placeholder="Procurar" aria-label="Search">
                             </form>  
 
-                            <ul class="menu-li col col-sm-10 col-lg navbar-nav m-auto mt-lg-0 mt-3 p-auto justify-content-end align-items-center">
+                            <ul class="menu-li col col-sm-10 col-lg navbar-nav m-auto mt-lg-0 mt-3 justify-content-end align-items-center">
                                 <li class="nav-item m-auto">
                                     <a class="nav-link i18 rounded-3 px-3" href="/pages/cadastro-campeonatos.html" key="LinkCadastroCampeonato">Criar Campeonato</a>
                                 </li>
