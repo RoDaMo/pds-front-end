f28209838f037122918ab78db8ed91a968c95ccc
Gustavo Ian <gustavoian1790@gmail.com>
2023-05-06 17:52:42 -0300

    feat: Implementando efeitos no título da home e arrumando features

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { inicializarInternacionalizacao } from './js/utilidades/internacionaliza
 import globalEn from './js/i18n/en/main.json' assert { type: 'JSON' };
 import globalPt from './js/i18n/ptbr/main.json' assert { type: 'JSON' };
 import '/scss/styles.scss'
+import 'bootstrap-icons/font/bootstrap-icons.scss'
 import * as bootstrap from 'bootstrap'
 
 if (!localStorage.getItem('lng')) {
