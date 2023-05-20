96e5d30207abbcc97de42e9e592618d78c85545b
Kaique Santos <kaique.santos20042018@gmail.com>
2023-05-20 11:04:18 -0300

    fix: verificacao de existencia de usuario

--- js/login.js
+++ js/login.js
@@ -8,6 +8,32 @@ const mensagemErro = document.getElementById("mensagem-erro")
 
 visualizarSenha()
 
+document.getElementById("continuar").addEventListener("click", async(e) => {
+    e.preventDefault();
+    await postUsuarioExiste({
+        "Username": nomeUsuario.value,
+    })
+})
+
+async function postUsuarioExiste(body) {
+    const config = configuracaoFetch("POST", body)
+    const res = await fetch(`https://playoffs-api.up.railway.app/auth/exists`, config)
+
+    const data = await res.json()
+    console.log(data)
+
+    if(data.results){
+        document.getElementById("continuar").classList.add("d-none")
+        document.getElementById("entrar").classList.remove("d-none")
+        document.getElementById("senha-formulario").classList.remove("d-none")
+    }
+    else{
+        window.location.assign("/pages/cadastro-usuarios.html");
+    }
+
+    return true
+}
+
 formulario.addEventListener("submit", async(e) => {
     e.preventDefault()
     limparMensagem(mensagemErro)
@@ -16,9 +42,6 @@ formulario.addEventListener("submit", async(e) => {
         "Username": nomeUsuario.value,
         "Password": senha.value,
     })
-
-    if (resultado)
-        formulario.reset()
 })
 
 async function postToken(body) {
@@ -31,6 +54,7 @@ async function postToken(body) {
     if(!data.succeed){
         mensagemErro.textContent = data.message
         mensagemErro.classList.add("text-danger")
+        senha.value = ""
     }
 
     return true
