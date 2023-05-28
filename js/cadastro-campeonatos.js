029c3e0cbc5e1f0233fde2658e137f0bf87580cb
Gustavo Ian <gustavoian1790@gmail.com>
2023-05-28 09:36:14 -0300

    fix: resolvendo conflitos (cadastro-campeonatos)

--- js/cadastro-campeonatos.js
+++ js/cadastro-campeonatos.js
@@ -3,83 +3,213 @@ import { notificacaoSucesso } from "./utilidades/notificacoes"
 import { inicializarInternacionalizacao } from "./utilidades/internacionalizacao"
 import portugues from './i18n/ptbr/cadastro-campeonatos.json' assert { type: 'JSON' }
 import ingles from './i18n/en/cadastro-campeonatos.json' assert { type: 'JSON' }
+
 import JustValidate from "just-validate"
+import flatpickr from "flatpickr"
+import { Portuguese } from "flatpickr/dist/l10n/pt.js"
+// import {exibidorImagem} from '../js/utilidades/previewImagem'
+
 
 inicializarInternacionalizacao(ingles, portugues);
 let formulario = document.getElementById("formulario")
-let mensagemErro = document.getElementById("mensagem-erro")
+const mensagemErro = document.getElementById("mensagem-erro")
+const escudo = document.getElementById('escudo')
+
+const pais = document.getElementById('pais')
+const estado = document.getElementById('estado')
+const cidade = document.getElementById('cidade')
+const bairro = document.getElementById('bairro')
+const descricao = document.getElementById('descricao')
+const dataInicial = document.getElementById("data-inicial")
+const dataFinal = document.getElementById("data-final")
+const nomeCampeonato = document.getElementById("nome")
+const esporte = document.getElementById("esportes")
+const formato = document.getElementById('formato')
+const quantidade = document.getElementById('quantidade')
+const imagem = document.getElementById('logo')
+
+// exibidorImagem(imagem, escudo)
+
+const optionDefault = () => {
+    const optionDefault = document.createElement('option')
+    optionDefault.value = 0
+    optionDefault.text = 'Selecione uma opção'
+    quantidade.appendChild(optionDefault)
+}
+
+const adicionarOpcao = (value) => {
+    const option = document.createElement('option')
+    option.value = value
+    option.text = value
+    quantidade.appendChild(option)
+}
+
+const resetQuantidade = () => {
+    quantidade.innerHTML = ""
+    optionDefault()
+}
+
+formato.addEventListener("change", () => {
+    if(formato.value === "1"){
+        resetQuantidade()
+        for(let i = 1; i <= 18; i++){
+            if(i % 2 === 0){
+                adicionarOpcao(i + 2)
+            }
+        }
+    }
+    else{
+        resetQuantidade()
+        for(let i = 1; i <= 6; i++){
+            adicionarOpcao(2 ** i)
+        }
+    }
+})
+
+flatpickr(dataInicial, {
+    dateFormat: "Y-m-d",
+    locale: Portuguese,
+    altInput: true,
+})
+
+flatpickr(dataFinal, {
+    dateFormat: "Y-m-d",
+    locale: Portuguese,
+    altInput: true,
+})
 
 const validator = new JustValidate(formulario, {
     validateBeforeSubmitting: true,
 })
 
 validator
-    .addField('#nome-campeonato', [
+    .addField(nomeCampeonato, [
         {
             rule: 'required',
             errorMessage: 'O nome do campeonato é obrigatório',
         },
     ])
-    .addField('#data-inicio', [
+    .addField(dataInicial, [
         {
             rule: 'required',
-            errorMessage: 'A data de início é obrigatória',
+            errorMessage: 'A data inicial é obrigatória',
         },
     ])
-    .addField('#data-final', [
+    .addField(dataFinal, [
         {
             rule: 'required',
             errorMessage: 'A data final é obrigatória',
         },
+        {
+            validator: (value, context) => {
+                const dataInicial = new Date(document.getElementById("data-inicial").value)
+                const dataFinal = new Date(value)
+                return dataFinal >= dataInicial
+            },
+            errorMessage: 'A data final deve ser maior ou igual a data inicial',
+        }
     ])
-    .addField('#esportes', [
+    .addField(esporte, [
         {
             rule: 'required',
             errorMessage: 'Favor selecionar um esporte',
         },
     ])
-    .addRequiredGroup('#grupo-premiacao', 'Selecione ao menos uma opção')
+    .addField(formato, [
+        {
+            rule: 'required',
+            errorMessage: 'Favor selecionar um formato',
+        },
+    ])
+    .addField(quantidade, [
+        {
+            rule: 'required',
+            errorMessage: 'Favor selecionar uma quantidade',
+        },
+    ])
+    .addField(imagem, [
+        {
+            rule: 'required',
+            errorMessage: 'Favor selecionar uma imagem',
+        },
+        {
+            rule: 'files',
+            value: {
+                files: {
+                    extensions: ['jpeg', 'jpg', 'png'],
+                    maxSize: 20000,
+                    types: ['image/jpeg', 'image/jpg', 'image/png'],
+                },
+            },
+            errorMessage: 'Imagem em jpeg/jpg/png - máx. 20kb',
+        }
+    ])
+    .addField(pais, [
+        {
+            rule: 'required',
+            errorMessage: 'O país é obrigatório',
+        },
+    ])
+    .addField(estado, [
+        {
+            rule: 'required',
+            errorMessage: 'O estado é obrigatório',
+        },
+    ])
+    .addField(cidade, [
+        {
+            rule: 'required',
+            errorMessage: 'A cidade é obrigatório',
+        },
+    ])
+    .addField(bairro, [
+        {
+            rule: 'required',
+            errorMessage: 'O bairro é obrigatório',
+        },
+    ])
+    .addField(descricao, [
+        {
+            rule: 'required',
+            errorMessage: 'Favor inserir uma descrição',
+        },
+    ])
     .onSuccess(async(e) => {
         e.preventDefault()
         limparMensagem(mensagemErro)
 
-        let nomeCampeonato = document.getElementById("nome-campeonato").value
-        let dataInicio = document.getElementById("data-inicio").value
-        let dataFinal = document.getElementById("data-final").value
-        let esporte = document.getElementById("esportes").value
-        let premiacao = document.querySelector('input[name="premiacao"]:checked').value
-
         const resultado = await postCampeonato("championships", {
-            "name": nomeCampeonato,
-            "prize": premiacao,
-            "initialDate": dataInicio,
-            "finalDate": dataFinal,
-            "sportsId": esporte
+            "name": nomeCampeonato.value,
+            "initialDate": dataInicial.value,
+            "finalDate": dataFinal.value,
+            "sportsId": parseInt(esporte.value),
+            "teamQuantity": parseInt(quantidade.value),
+            "logo": imagem.value,
+            "description": descricao.value,
+            "Format": parseInt(formato.value),
+            "Nation": pais.value,
+            "State": estado.value,
+            "City": cidade.value,
+            "Neighborhood": bairro.value
         })
 
-        if (resultado)
+        if (resultado) {
             formulario.reset()
+            escudo.src = "#"
+        }
     })
 
 async function postCampeonato(endpoint, body) {
+    console.log(body)
     const config = configuracaoFetch("POST", body)
+
     const callbackServidor = data => {
         mensagemErro.classList.add("text-danger")
         data.results.forEach(element => mensagemErro.innerHTML += `${element}<br>`);
     }
 
-    const data = await executarFetch(endpoint, config, null, callbackServidor)
+    const data = await executarFetch(endpoint, config, (res) => mensagemErro.textContent = res.results[0], callbackServidor)
     if (!data) return false
-
-    // if (!data.succeed) {
-    //     mensagemErro.classList.add("text-danger")
-    //     mensagemErro.textContent = data.results[0]
-    //     return false
-    //     // nao permita que o label desapareca
-    //     // setTimeout(() => {
-    //     //     mensagemErro.textContent = ""
-    //     // }, 3500);
-    // }
 
     notificacaoSucesso(data.results[0])
     return true
