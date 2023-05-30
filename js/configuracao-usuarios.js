f5ded51aabe89ca412ed36696e28071094cb86b0
Gustavo Ian <gustavoian1790@gmail.com>
2023-05-30 15:45:47 -0300

    feat: definindo estrutura da pág de configuração

--- js/configuracao-usuarios.js
+++ js/configuracao-usuarios.js
@@ -1,1 +1,23 @@
 import '../scss/configuracao-usuarios.scss'
+
+const configMenuList = document.querySelector('.config-menu-list')
+const configTitle = document.querySelector('.config-title')
+
+configMenuList.addEventListener('click', e => {
+    let target = e.target
+
+    if (target.tagName !== 'BUTTON') return
+
+    activateLi(target)
+
+    configTitle.innerText = target.innerText
+
+})
+
+function activateLi(li) {
+    for (let item of configMenuList.children) {
+        item.classList.remove('active')
+    }
+
+    li.classList.add('active')
+}
