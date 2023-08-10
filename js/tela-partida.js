efce3dca9aaefa139bf343f3a48f2cac9c58399b
Gustavo Ian <gustavoian1790@gmail.com>
2023-08-10 13:50:55 -0300

    feat: incluindo arquivos da tela de partida

---
+++ js/tela-partida.js
@@ -0,0 +1,13 @@
+import '../scss/tela-partida.scss'
+import { configuracaoFetch, executarFetch, limparMensagem } from "./utilidades/configFetch"
+import './utilidades/loader'
+import portugues from './i18n/ptbr/tela-partida.json' assert { type: 'JSON' }
+import ingles from './i18n/en/tela-partida.json' assert { type: 'JSON' }
+import i18next from "i18next";
+import { inicializarInternacionalizacao } from "./utilidades/internacionalizacao"
+
+inicializarInternacionalizacao(ingles, portugues);
+const loader = document.createElement('app-loader');
+document.body.appendChild(loader);
+
+
