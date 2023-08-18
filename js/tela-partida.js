f1f85a2aeaebfb3c5455aecb333a54129fe420a8
Gustavo Ian <gustavoian1790@gmail.com>
2023-08-18 16:03:44 -0300

    fix: mudando lógica dos eventos

--- js/tela-partida.js
+++ js/tela-partida.js
@@ -40,9 +40,6 @@ const init = async () => {
 
 		let eventEndpoint = ''
 
-		// true = yellowCard, false = redCard
-		let cardType = (selectEventType.value == 2) ? true : (selectEventType.value == 3) ? false : ''
-
 		selectEventType.addEventListener('change', () => {
 			// eventEndpoint = (selectEventType.value == 1) ? 'matches/goals' 
 			// 	: (selectEventType.value == 2 || selectEventType == 3) ? 'matches/cards'
@@ -68,6 +65,7 @@ const init = async () => {
 				})
 
 				switch (selectEventType.value) {
+					// Goal
 					case 1:
 						if (selectEventTeam.value) {
 							matchManagementForm.insertAdjacentHTML('beforeend', `
@@ -133,7 +131,46 @@ const init = async () => {
 								})
 						}
 						break;
-				
+					// Card 
+					case 2 || 3:
+							if (selectEventTeam.value) {
+								// true = yellowCard, false = redCard
+								let cardType = (selectEventType.value == 2) ? true : (selectEventType.value == 3) ? false : ''
+
+								matchManagementForm.insertAdjacentHTML('beforeend', `
+									<label for="select-event-player" class="i18 form-label" key="SelectEventPlayerLabel">${i18next.t("SelectEventPlayerLabel")}</label>
+									<select id="select-event-player" class="form-select">
+										<option value="" selected class="i18" key="SelectEventPlayerPlaceholder">${i18next.t("SelectEventPlayerPlaceholder")}</option>
+										${players.map(player => `<option value="${player.id}">${player.name}</option>`)}
+									</select>
+								`)
+
+								const selectEventPlayer = matchManagementForm.querySelector('select#select-event-player')
+
+								const postCardValidator = new JustValidate(matchManagementForm, {
+									validateBeforeSubmitting: true,
+								})
+
+								postCardValidator
+									.addField(selectEventPlayer, [
+										{
+											rule: 'required',
+											errorMessage: `<span class="i18" key="JogadorObrigatorio">${i18next.t("JogadorObrigatorio")}</span>`,
+										}
+									])
+									.onSuccess(async e => {
+										e.preventDefault()
+
+										loader.show()
+										await postCard({
+											"MatchId": match.id,
+											"PlayerTempId": selectEventPlayer.value,
+											"TeamId": selectEventTeam.value,
+											"CardType": cardType
+										})
+										loader.hide()
+									})
+							}
 					default:
 						break;
 				}
@@ -299,57 +336,144 @@ const init = async () => {
 				matchManagementSystem()
 			}
 
-			// Get all match informations
+			// Clear blurwall 
 			eventsWrapperTeam1.innerHTML = ''
 			eventsWrapperTeam2.innerHTML = ''
 
-			// Goals
+			// How to order all the events?
+			// Time Marking
+			// 1. Get all goals
+			// 2. Get all cards
+			// 3. Put them in the same array
+			// 4. Order the array by time (earliest to latest)
+			// 5. Loop through the array
+			// 6. Verify if the event is from team 1 or team 2
+				// 6.1. If it's a team 1 event, add the event to the team 1 eventsWrapper and add a blank space on the team 2 eventsWrapper
+					// 6.1.1. If it's a goal, insert the goal event template
+					// 6.1.2. If it's a card, insert the card event template
+						// 6.1.2.1. If it's a red card, insert the red card event icon
+						// 6.1.2.2. If it's a yellow card, insert the yellow card event icon
+				// 6.2. If it's a team 2 event, add the event to the team 2 eventsWrapper and add a blank space on the team 1 eventsWrapper
+					// 6.2.1. If it's a goal, insert the goal event template
+					// 6.2.2. If it's a card, insert the card event template
+						// 6.2.2.1. If it's a red card, insert the red card event icon
+						// 6.2.2.2. If it's a yellow card, insert the yellow card event icon
+						
+			// Get all goals
 			const
 				matchGoals = await executarFetch(`matches/${matchId}/goals`, configuracaoFetch('GET')),
 				matchGoalsResults = matchGoals.results
 
-			matchGoalsResults.forEach(result => {
-				if (result.teamId == match[0].id) {
-					eventsWrapperTeam1.insertAdjacentHTML('beforeend', `
-						<div class="row flex-column p-3 my-2 match-details-content-event rounded-5 position-relative">
-							<div class="col event-player-name"><span class="fw-semibold text-black text-truncate d-block">Goal Maker</span></div>
-							<div class="col d-flex flex-row event-data">
-								<div class="event-type"><span class="text-muted">Gol</span></div>
-								<i class="bi bi-dot"></i>
-								<div class="event-time"><span class="text-muted">"32</span></div>
+			// Get all cards
+			const
+				matchCards = await executarFetch(`matches/${matchId}/cards`, configuracaoFetch('GET')),
+				matchCardsResults = matchCards.results
+			
+			// Put them in the same array
+			let matchEvents = []
+			matchGoalsResults.forEach(goal => {
+				goal.type = 'goal'
+				matchEvents.push(goal)
+			})
+			matchCardsResults.forEach(card => {
+				card.type = 'card'
+				matchEvents.push(card)
+			})
+			
+			// Order the array by time (earliest to latest)
+			matchEvents.sort((a, b) => {
+				return a.time - b.time
+			})
+
+			itsGoal = false
+			itsCard = false
+
+			// Loop through the array
+			matchEvents.forEach(event => {
+				// Todo
+					// Get the player name
+					// Get the event time
+					// Change the event icon based on the event type
+					// Fix templates 
+					
+				// Verify if the event is from team 1 or team 2
+
+				// If it's a team 1 event, add the event to the team 1 eventsWrapper and add a blank space on the team 2 eventsWrapper
+				if (event.teamId == match[0].id) {
+					// If it's a goal, insert the goal event template
+					if (event.type == 'goal') {
+						eventsWrapperTeam1.insertAdjacentHTML('beforeend', `
+							<div class="row flex-column p-3 my-2 match-details-content-event rounded-5 position-relative">
+								<div class="col event-player-name"><span class="fw-semibold text-black text-truncate d-block">${player.name}</span></div>
+								<div class="col d-flex flex-row event-data">
+									<div class="event-type"><span class="text-muted i18" key="Gol">${i18next.t("Gol")}</span></div>
+									<i class="bi bi-dot"></i>
+									<div class="event-time"><span class="text-muted">"32</span></div>
+								</div>
+								<div class="col position-absolute w-auto h-auto event-illustration">
+									<img src="../public/icons/sports_soccer.svg" alt="">
+								</div>
 							</div>
-							<div class="col position-absolute w-auto h-auto event-illustration">
-								<img src="../public/icons/sports_soccer.svg" alt="">
+						`)
+
+					} else if (event.type == 'card') {
+						eventsWrapperTeam1.insertAdjacentHTML('beforeend', `
+							<div class="row flex-column p-3 my-2 match-details-content-event rounded-5 position-relative">
+								<div class="col event-player-name"><span class="fw-semibold text-black text-truncate d-block">Goal Maker</span></div>
+								<div class="col d-flex flex-row event-data">
+									<div class="event-type"><span class="text-muted i18" key="Falta">${i18next.t("Falta")}</span></div>
+									<i class="bi bi-dot"></i>
+									<div class="event-time"><span class="text-muted">"32</span></div>
+								</div>
+								<div class="col position-absolute w-auto h-auto event-illustration">
+									<img src="../public/icons/sports_soccer.svg" alt="">
+								</div>
 							</div>
-						</div>
-					`)
+						`)
+					}
 
 					eventsWrapperTeam2.insertAdjacentHTML('beforeend', `
 						<div class="row blank-space"></div>
 					`)
-				}
-
-				if (result.teamId == match[1].id) {
-					eventsWrapperTeam2.insertAdjacentHTML('beforeend', `
-						<div class="row flex-column p-3 my-2 match-details-content-event rounded-5 position-relative">
-							<div class="col event-player-name"><span class="fw-semibold text-black text-truncate d-block">Goal Maker</span></div>
-							<div class="col d-flex flex-row event-data">
-								<div class="event-type"><span class="text-muted">Gol</span></div>
-								<i class="bi bi-dot"></i>
-								<div class="event-time"><span class="text-muted">"32</span></div>
-							</div>
-							<div class="col position-absolute w-auto h-auto event-illustration">
-								<img src="../public/icons/sports_soccer.svg" alt="">
-							</div>
-						</div>
-					`)
-
-					eventsWrapperTeam1.insertAdjacentHTML('beforeend', `
-						<div class="row blank-space"></div>
-					`)
-				}
 				
-			});
+				// If it's a team 2 event, add the event to the team 2 eventsWrapper and add a blank space on the team 1 eventsWrapper
+				} else if (event.teamId == match[1].id) {
+					if (event.type == 'goal') {
+						eventsWrapperTeam2.insertAdjacentHTML('beforeend', `
+							<div class="row flex-column p-3 my-2 match-details-content-event rounded-5 position-relative">
+								<div class="col event-player-name"><span class="fw-semibold text-black text-truncate d-block">Goal Maker</span></div>
+								<div class="col d-flex flex-row event-data">
+									<div class="event-type"><span class="text-muted i18" key="Gol">${i18next.t("Gol")}</span></div>
+									<i class="bi bi-dot"></i>
+									<div class="event-time"><span class="text-muted">"32</span></div>
+								</div>
+								<div class="col position-absolute w-auto h-auto event-illustration">
+									<img src="../public/icons/sports_soccer.svg" alt="">
+								</div>
+							</div>
+						`)
+
+					} else if (event.type == 'card') {
+						eventsWrapperTeam2.insertAdjacentHTML('beforeend', `
+							<div class="row flex-column p-3 my-2 match-details-content-event rounded-5 position-relative">
+								<div class="col event-player-name"><span class="fw-semibold text-black text-truncate d-block">Goal Maker</span></div>
+								<div class="col d-flex flex-row event-data">
+									<div class="event-type"><span class="text-muted i18" key="Falta">${i18next.t("Falta")}</span></div>
+									<i class="bi bi-dot"></i>
+									<div class="event-time"><span class="text-muted">"32</span></div>
+								</div>
+								<div class="col position-absolute w-auto h-auto event-illustration">
+									<img src="../public/icons/sports_soccer.svg" alt="">
+								</div>
+							</div>
+						`)
+					}	
+
+					eventsWrapperTeam1.insertAdjacentHTML('beforeend', `
+						<div class="row blank-space"></div>
+					`)
+				}
+			})
 		}
 	}
 
